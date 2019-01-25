083fe2e618ec9d4549b673a21862409574cdca61
funshofab <funchikel@gmail.com>
2019-01-25 20:15:20 +0100

    Build starter app
    The app contains a username EditText and a register Button.
    The length of the username entered is watched. If the length meets the required minimum length for a username, the register button is enabled else it is disabled

---
+++ README.md
@@ -0,0 +1,13 @@
+Gdg Ogbomosho Codelab
+=
+This is a simple app built for my Android Jetpack codelab session at GDG Ogbomosho, 2019.
+
+## Goal
+To show basic use of the following:
+- DataBinding
+- ViewModel
+- LiveData
+- Room
+
+## Audience
+Everyone with basic knowledge of Android development using Java.
--- app/src/main/java/com/funshofab/gdgcodelab/MainActivity.java
+++ app/src/main/java/com/funshofab/gdgcodelab/MainActivity.java
@@ -1,14 +1,57 @@
 package com.funshofab.gdgcodelab;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // The minimum number of characters allowed for a username
+    private final int MINIMUM_CHARACTERS_ALLOWED = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeWidgets();
+    }
+
+    private void initializeWidgets() {
+        // Get reference to the widgets
+        TextInputEditText usernameEditText = findViewById(R.id.username_edit_text);
+        final Button registerButton = findViewById(R.id.register_button);
+
+        // Disable the register button until a username longer than MINIMUM_CHARACTERS_ALLOWED characters is entered
+        registerButton.setEnabled(false);
+
+        // Listen for changes on the username EditText
+        usernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Ignore this
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Check if the length of the text is greater than or equal to MINIMUM_CHARACTERS_ALLOWED.
+                // If it is, enable the register button
+                if (s.length() >= MINIMUM_CHARACTERS_ALLOWED) {
+                    registerButton.setEnabled(true);
+                } else {
+                    registerButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Ignore this
+            }
+        });
     }
 }
